fe874211bfd5055f6b69b5a4e2147413e3ddc155
左云飞 <yunfei@tju.edu.cn>
2022-10-22 17:48:09 +0800

    Update CodeOS.jsx

--- src/info/CodeOS.jsx
+++ src/info/CodeOS.jsx
@@ -3,33 +3,16 @@ import { Typography } from "@douyinfe/semi-ui";
 const CodeOS = () => {
   const { Text, Paragraph } = Typography;
   return (
-    <div style={{ margin: "8px" }}>
+    <div style={{ margin: "8px", display: "flex", flexDirection: "column" }}>
       <Paragraph>此站点主要基于以下开源软件：</Paragraph>
       <Text link={{ href: "https://zh-hans.reactjs.org/" }}>React.js</Text>
-      <Text
-        style={{ marginLeft: "8px" }}
-        link={{ href: "http://semi.design/" }}
-      >
-        Semi-Design
-      </Text>
-      <Text
-        style={{ marginLeft: "8px" }}
-        link={{ href: "https://github.com/LiosK/UUID.js" }}
-      >
-        UUID.js
-      </Text>
-      <Text
-        style={{ marginLeft: "8px" }}
-        link={{ href: "https://github.com/sudodoki/copy-to-clipboard" }}
-      >
+      <Text link={{ href: "http://semi.design/" }}>Semi-Design</Text>
+      <Text link={{ href: "https://github.com/LiosK/UUID.js" }}>UUID.js</Text>
+      <Text link={{ href: "https://github.com/sudodoki/copy-to-clipboard" }}>
         copy-to-clipboard
       </Text>
-      <Text
-        style={{ marginLeft: "8px" }}
-        link={{ href: "https://day.js.org/" }}
-      >
-        Day.js
-      </Text>
+      <Text link={{ href: "https://day.js.org/" }}>Day.js</Text>
+      <Text link={{ href: "https://highlightjs.org/" }}>highlight.js</Text>
     </div>
   );
 };
