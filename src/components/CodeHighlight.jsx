84a25c03ad53006b6cf999311a1152c9f4d5d1a0
左云飞 <yunfei@tju.edu.cn>
2022-10-22 16:47:54 +0800

    fix bug

--- src/components/CodeHighlight.jsx
+++ src/components/CodeHighlight.jsx
@@ -3,12 +3,16 @@ import "highlight.js/styles/github.css";
 
 const CodeHighlight = ({ language = "text", content }) => {
   return (
-    <pre
-      style={{ fontFamily: "JetBrainsMono", fontSize: "14px" }}
-      dangerouslySetInnerHTML={{
-        __html: hljs.highlight(content, { language: language }).value,
-      }}
-    ></pre>
+    <>
+      {content ? (
+        <pre
+          style={{ fontFamily: "JetBrainsMono", fontSize: "14px" }}
+          dangerouslySetInnerHTML={{
+            __html: hljs.highlight(content, { language: language }).value,
+          }}
+        ></pre>
+      ) : null}
+    </>
   );
 };
 
