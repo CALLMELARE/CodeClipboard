47a8a15610a8f2bc76a0f984e7e1dacb77a4b476
左云飞 <yunfei@tju.edu.cn>
2022-10-28 16:16:45 +0800

    [feat]新增支持语言
    ruby,dockerfile,nginx,apache,matlab,dart,django,arduino,sql

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -1,8 +1,13 @@
+import IconApache from "./Apache.png";
+import IconArduino from "./Arduino.png";
 import IconBash from "./Bash.png";
 import IconC from "./C.png";
 import IconCSharp from "./CSharp.png";
 import IconCSS from "./CSS.png";
 import IconCpp from "./Cpp.png";
+import IconDart from "./Dart.png";
+import IconDjango from "./Django.png";
+import IconDockerfile from "./Dockerfile.png";
 import IconGo from "./Golang.png";
 import IconGraphQL from "./GraphQL.png";
 import IconHTML from "./HTML.png";
@@ -11,8 +16,12 @@ import IconJavaScript from "./JavaScript.png";
 import IconJSON from "./JSON.png";
 import IconKotlin from "./Kotlin.png";
 import IconMarkdown from "./Markdown.png";
+import IconMatlab from "./Matlab.png";
+import IconNginx from "./Nginx.png";
 import IconPython from "./Python.png";
+import IconRuby from "./Ruby.png";
 import IconSCSS from "./SCSS.png";
+import IconSQL from "./SQL.png";
 import IconSwift from "./Swift.png";
 import IconTypeScript from "./TypeScript.png";
 
@@ -29,11 +38,16 @@ const Icon = ({ src, size = "1em", style, ...props }) => {
 
 export default Icon;
 export {
+  IconApache,
+  IconArduino,
   IconBash,
   IconC,
   IconCpp,
   IconCSharp,
   IconCSS,
+  IconDart,
+  IconDjango,
+  IconDockerfile,
   IconGo,
   IconGraphQL,
   IconHTML,
@@ -42,8 +56,12 @@ export {
   IconJSON,
   IconKotlin,
   IconMarkdown,
+  IconMatlab,
+  IconNginx,
   IconPython,
+  IconRuby,
   IconSCSS,
+  IconSQL,
   IconSwift,
   IconTypeScript,
 };
